09bd6c4c3ff9880ee6d9de685770e95fdcc72efc
Giacomo Perticone <giacomo_mauro_perticone@yahoo.it>
2022-11-30 17:07:57 +0000

    installed chi

--- main.go
+++ main.go
@@ -5,13 +5,28 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>welcome to my awesome site</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch, email me at <a href=\"mailto:giacoomo_mauro_perticone@yahoo.it\">giacomo_mauro_perticone@yahoo.it</a>.</p>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>FAQ</h1>")
+	fmt.Fprintln(w, "Question1?")
+	fmt.Fprintln(w, "<p>Answer1</p>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/faq", faqHandler)
+
 	fmt.Println("Stating the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
